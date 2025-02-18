ecc2046ae4fc8e573fcb87ce3b4a62a9a225e428
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-18 13:33:00 +0700

    feat: matching syntax

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -8,31 +8,37 @@ const product = ref(null);
 const route = useRoute();
 
 watchEffect(() => {
-    fetch(`/api/products/${route.params.id}.json`)
-    .then(res => res.json())
-    .then(data => {
-        product.value = data;
-        loaded.value = true;
-    })
-    .catch(err => {
-        console.error(`error loading product: ${err}`);
-        err.value = true;
-    }); 
+    if (route.params.id) {
+        fetch(`/api/products/${route.params.id}.json`)
+            .then(res => res.json())
+            .then(data => {
+                product.value = data;
+                loaded.value = true;
+            })
+            .catch(err => {
+                console.error(`error loading product: ${err}`);
+                error.value = true;
+            });
+    }
 })
 </script>
-        
+
 <template>
-    <div v-if="loaded">
-        <h1>{{ product.id }} - {{ product.name  }}</h1>
-        <p>Price: {{ product.price }}</p>
-    </div>
-    <div v-else-if="error">
-        <h1>Error loading products: {{ route.params.id }}</h1>
-    </div>
-    <div v-else>
-        <h1>Loading products: {{ route.params.id }}</h1>
-    </div>
+    <template v-if="route.params.id">
+        <div v-if="loaded">
+            <h1>{{ product.id }} - {{ product.name }}</h1>
+            <p>Price: {{ product.price }}</p>
+        </div>
+        <div v-else-if="error">
+            <h1>Error loading products: {{ route.params.id }}</h1>
+        </div>
+        <div v-else>
+            <h1>Loading products: {{ route.params.id }}</h1>
+        </div>
+    </template>
+    <template v-else>
+        <h1>No products selected.</h1>
+    </template>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Home from './components/Home.vue'
 import About from './components/About.vue'
 import ProductDetail from './components/ProductDetail.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import NotFound from './components/NotFound.vue'
 
 const router = createRouter({
     routes: [
@@ -14,10 +15,20 @@ const router = createRouter({
         {
             path: "/about",
             component: About,
+            // Sensitive route
+            sensitive: true,
         },
         {
-            path: "/products/:id",
+            // Matching syntax route
+            // Tanda ? untuk optional param
+            path: "/products/:id(\\d+)?",
             component: ProductDetail,
+            sensitive: true,
+        },
+        {
+            // Repeatable route
+            path: "/:notFound*",
+            component: NotFound,
         }
     ],
     history: createWebHistory(),
