820dba77711c6b3d25c2f840855f1094cbd47064
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-18 15:24:41 +0700

    feat: navigation

--- src/components/User.vue
+++ src/components/User.vue
@@ -4,11 +4,25 @@
 
 <template>
     <h1>User</h1>
-    <div>Put header here</div>
+
+    <ul>
+        <li><RouterLink to="/users/profile">User Profile </RouterLink></li>
+        <li><RouterLink to="/users/order">User Order</RouterLink></li>
+        <li><RouterLink to="/users/wishlist">User Wishlist</RouterLink></li>
+    </ul>
+
     <RouterView/>
     <div>Put footer here</div>
 </template>
 
 <style scoped>
-
+/* RouterLink akan menambahkan class router-link-active & router-link-exact-active */
+/* .router-link-active {
+    color: red;
+    font-weight: bold;
+} */
+.router-link-exact-active {
+    color: red;
+    font-weight: bold;
+}
 </style>
